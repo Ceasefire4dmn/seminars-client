6b8a09007db0791996d8c77f629b0cd7d5ccbf85
Aleksandra <alpha4ka.2005@gmail.com>
2025-02-10 01:35:54 -0500

    Add styles media

--- src/entities/SeminarItem.jsx
+++ src/entities/SeminarItem.jsx
@@ -7,7 +7,7 @@ const SeminarItem = ({ seminar, onEdit, onDelete }) => {
         <Accordion.Item eventKey={seminar.id}>
             <Accordion.Header>{seminar.title} ({seminar.date})</Accordion.Header>
             <Accordion.Body>
-                <Card className="d-flex flex-row seminar-body ">
+                <Card className="d-flex flex-row">
                     <Card.Img
                         className="p-2"
                         variant="top"
@@ -16,7 +16,7 @@ const SeminarItem = ({ seminar, onEdit, onDelete }) => {
                         style={{ height: "300px", width: "300px" }}
                     />
                     <Card.Body className="d-flex flex-column justify-content-between">
-                        <Card.Text className="fs-4">
+                        <Card.Text className="fs-4" style={{wordBreak: 'break-all'}}>
                             {seminar.description}
                         </Card.Text>
                         <p className="fs-6">
